6a9721efb1ab8368d2507769b7510b79cb2b5aa8
HaseebH252 <36900904+HaseebH252@users.noreply.github.com>
2022-08-20 14:28:53 -0400

    Update uninstaller.bat

--- Batch/uninstaller.bat
+++ Batch/uninstaller.bat
@@ -1,6 +1,6 @@
 @Echo OFF
 set application=""
-echo Uninstalling any old versions of 8x8....
+echo Uninstalling any versions of %application%....
 powershell.exe "Get-Package -Name *%application%* | Uninstall-Package -Force -ErrorAction silentlycontinue"
 echo Uninstalled!
 
